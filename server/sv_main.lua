44f7cb5055285db4a59f926ad94f9050933eac50
wolfex <91018450+wolfex01@users.noreply.github.com>
2023-02-14 21:55:45 +0100

    Update sv_main.lua
    A szerveroldali kód átalakítása során átneveztem a globális változót ESX-ről DvESX-re, hogy ne ütközzön más használt ESX verziókkal. Hozzáadtam a RegisterServerEvent() hívást, amely figyeli a dv_licenses:buy eseményt. Amikor a szerver ezt az eseményt érzékeli, akkor az eseménykezelő függvény ellenőrzi, hogy a játékosnak van-e elég pénze a licenc megvásárlásához. Ha van, akkor a megfelelő licencet hozzáadjuk az inventáriumához, és levonjuk az árát a játékos számlájáról. Ha nincs elegendő pénze, akkor értesítést küldünk a játékosnak. A kód átdolgozása során átalakítottam az értesítési rendszert, hogy támogassa a DvNotify-t, ha azt a konfigurációs fájlban bekapcsoljuk.

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,34 +1,34 @@
-ESX = nil
+local DvESX = nil
 
-TriggerEvent(Config.SharedObject, function(obj) ESX = obj end)
+TriggerEvent('esx:getSharedObject', function(obj) DvESX = obj end)
 
 RegisterServerEvent('dv_licenses:buy')
 AddEventHandler('dv_licenses:buy', function(type, price)
-    local xPlayer = ESX.GetPlayerFromId(source)
+    local xPlayer = DvESX.GetPlayerFromId(source)
 
     if xPlayer.getMoney() >= price then
         if type == "DriverLicense" then
             xPlayer.removeMoney(Config.DriverLicense.item_price)
             xPlayer.addInventoryItem(Config.DriverLicense.item_name, 1)
             if Config.UseDvNotify then
-                TriggerClientEvent('dv_notify:alert', source, "You successfully bought a " ..ESX.GetItemLabel(Config.DriverLicense.item_name).."!", 5000, "71FF33", true)
+                TriggerClientEvent('dv_notify:alert', source, "You successfully bought a " ..DvESX.GetItemLabel(Config.DriverLicense.item_name).."!", 5000, "71FF33", true)
             else
-                xPlayer.showNotification("You successfully bought a " ..ESX.GetItemLabel(Config.DriverLicense.item_name).."!")
+                xPlayer.showNotification("You successfully bought a " ..DvESX.GetItemLabel(Config.DriverLicense.item_name).."!")
             end
         else
             xPlayer.removeMoney(Config.CardLicense.item_price)
             xPlayer.addInventoryItem(Config.CardLicense.item_name, 1)
             if Config.UseDvNotify then
-                TriggerClientEvent('dv_notify:alert', source, "You successfully bought a " ..ESX.GetItemLabel(Config.CardLicense.item_name).."!", 5000, "71FF33", true)
+                TriggerClientEvent('dv_notify:alert', source, "You successfully bought a " ..DvESX.GetItemLabel(Config.CardLicense.item_name).."!", 5000, "71FF33", true)
             else
-                xPlayer.showNotification("You successfully bought a " ..ESX.GetItemLabel(Config.CardLicense.item_name).."!")
+                xPlayer.showNotification("You successfully bought a " ..DvESX.GetItemLabel(Config.CardLicense.item_name).."!")
             end  
         end
     else
         if Config.UseDvNotify then
-            TriggerClientEvent('dv_notify:alert', source, "You don't have enought money!", 5000, "d31f37", true)
+            TriggerClientEvent('dv_notify:alert', source, "You don't have enough money!", 5000, "d31f37", true)
         else
-            xPlayer.showNotification("You don't have enought money!")
+            xPlayer.showNotification("You don't have enough money!")
         end  
     end
 end)
